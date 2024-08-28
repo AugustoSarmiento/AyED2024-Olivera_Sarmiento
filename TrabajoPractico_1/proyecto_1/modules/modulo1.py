a5bb9c6638a6de5ace01e3180d78e9a76ec99bd4
OPERADOR <julietaoliveravilla@gmail.com>
2024-08-28 20:55:05 -0300

    Agregué algoritmo de ordenamiento Quicksort

--- TrabajoPractico_1/proyecto_1/modules/modulo1.py
+++ TrabajoPractico_1/proyecto_1/modules/modulo1.py
@@ -4,3 +4,21 @@ def bubble_sort(lista):
             if lista[j] > lista[j+1]:
                 lista[j], lista[j+1] = lista[j+1],lista[j]
     return(lista)
+
+    def quick_sort(lista):
+    largo = len(lista)
+    if largo <= 1:
+        return lista
+    else:
+        pivote = lista.pop()
+        
+    mayores = []
+    menores = []
+    
+    for i in lista:
+        if i > pivote:
+            mayores.append(i)
+        
+        else:
+            menores.append(i)
+    return quick_sort(menores) + [pivote] + quick_sort(mayores)
