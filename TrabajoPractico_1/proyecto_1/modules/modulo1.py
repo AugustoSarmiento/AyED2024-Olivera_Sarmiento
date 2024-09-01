6686e12d4fd42b882d414d12503fec87a7051fab
AUGUSTO <augusto.sar7704@gmail.com>
2024-09-01 17:04:34 -0300

    Implementación de radix sort

--- TrabajoPractico_1/proyecto_1/modules/modulo1.py
+++ TrabajoPractico_1/proyecto_1/modules/modulo1.py
@@ -5,7 +5,7 @@ def bubble_sort(lista):
                 lista[j], lista[j+1] = lista[j+1],lista[j]
     return(lista)
 
-    def quick_sort(lista):
+def quick_sort(lista):
     largo = len(lista)
     if largo <= 1:
         return lista
@@ -22,3 +22,27 @@ def bubble_sort(lista):
         else:
             menores.append(i)
     return quick_sort(menores) + [pivote] + quick_sort(mayores)
+
+def radix_sort(lista):
+    max_num = max(lista)
+    exp = 1
+
+    while max_num / exp > 0:
+        bloques = [[] for _ in range(10)]
+        for i in lista:
+            digito = (i // exp) % 10
+            bloques[digito].append(i)
+        lista = []
+        for bloque in bloques:
+            lista.extend(bloque)
+        exp *= 10
+    return lista
+
+import random
+lista_prueba=[]
+for i in range(500):
+  x= random.randint(10000,99999)
+  lista_prueba.append(x)
+print(lista_prueba)
+print(bubble_sort(lista_prueba))
+print(radix_sort(lista_prueba))
