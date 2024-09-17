fd2a19a9b5f9dc10d6fe68528385e633fad32cde
AUGUSTO <augusto.sar7704@gmail.com>
2024-09-17 16:11:05 -0300

    Agregamos los métodos que faltaban de la actividad 2. Logramos que las pruebas corran en main.py, algunos errores que no sabemos interpretar.

--- TrabajoPractico_1/Actividad_2/modules/ListasDoblementeEnlazadas.py
+++ TrabajoPractico_1/Actividad_2/modules/ListasDoblementeEnlazadas.py
@@ -1,90 +1,144 @@
 #consigna 2
 class Nodo:
-    def __init__(self, dato):
-        self.data = dato
-        self.prev = None
-        self.next = None
+    def __init__(self, data):
+        self.data = data
+        self.anterior = None
+        self.siguiente = None
 
 class ListaDoblementeEnlazada:
     def __init__(self):
         self.cabeza = None
+        self.cola =None
         self.tamanio =0
 
-    def agregar_al_final(self, dato):
-        nuevo_nodo = Nodo(dato)
+    def agregar_al_final(self, item):
+        nuevo_nodo = Nodo(item)
         if self.cabeza is None:
             self.cabeza = nuevo_nodo
+            self.cola =nuevo_nodo
 
         else:
-            ultimo_nodo = self.cabeza
-            while ultimo_nodo.next:
-                ultimo_nodo = ultimo_nodo.next
-            ultimo_nodo.next = nuevo_nodo
-
-            nuevo_nodo.prev = ultimo_nodo
-        self.tamanio +=1
+            self.cola.siguiente = nuevo_nodo
+            nuevo_nodo.anterior = self.cola
+            self.cola = nuevo_nodo  
+        self.tamanio += 1
     
-    def agregar_al_principio(self, dato):
-        nuevo_nodo = Nodo(dato)
+    def agregar_al_inicio(self, item):
+        nuevo_nodo = Nodo(item)
         if self.cabeza is None:
             self.cabeza = nuevo_nodo
         else:
-            self.cabeza.prev = nuevo_nodo
-            nuevo_nodo.next = self.cabeza
+            self.cabeza.anterior = nuevo_nodo
+            nuevo_nodo.siguiente = self.cabeza
             self.cabeza = nuevo_nodo
         self.tamanio+=1
             
-    def mostrar_lista(self):
-        cursor = self.cabeza
-        while cursor:
-            print(cursor.data, end=" ")
-            cursor = cursor.next
-
-    def esta_vacia(self): #si la lista está vacía, la cabeza no tiene valor
+    def esta_vacia(self): 
         if self.cabeza == None:
             return (print(True))
         else:
             return (print(False))
-        
+               
     def __len__(self):
         cant_elementos = self.tamanio
-        return(print(cant_elementos))
+        return
     
-    def insertar(self, dato, posicion=None):
+    def insertar(self, item, posicion=None):
         if posicion is None:
-            self.agregar_al_final(dato)
-            return
+            self.agregar_al_final(item)
+            
         if posicion < 0 or posicion > self.tamanio:
-            raise ValueError("Posición fuera de rango")
+            raise ValueError("La posición dada no está incluída en la lista.")
 
         if posicion == 0:
-            self.agregar_al_principio(dato)
-            return
+            self.agregar_al_principio(item)
 
         if posicion == self.tamanio:
-            self.agregar_al_final(dato)
-            return
+            self.agregar_al_final(item)
 
-        nuevo_nodo = Nodo(dato)
+        nuevo_nodo = Nodo(item)
         actual = self.cabeza
         for i in range(posicion - 1):
-            actual = actual.next
+            actual = actual.siguiente
 
-        nuevo_nodo.prev = actual
-        nuevo_nodo.next = actual.next
-        actual.next.prev = nuevo_nodo
-        actual.next = nuevo_nodo
+        nuevo_nodo.anterior = actual
+        nuevo_nodo.siguiente = actual.siguiente
+        actual.siguiente.anterior = nuevo_nodo
+        actual.siguiente = nuevo_nodo
 
         self.tamanio += 1
-        
-lista=ListaDoblementeEnlazada()
-lista.esta_vacia()
-lista.agregar_al_final(0)
-lista.agregar_al_principio(4)
-lista.agregar_al_principio(1)
-lista.agregar_al_final(2)
-lista.insertar(5,3)
-lista.insertar(8)
-lista.esta_vacia()
-lista.__len__()
-lista.mostrar_lista()
+
+    def extraer (self, posicion = None):
+        if self.tamanio == 0:
+            raise IndexError("La lista está vacía")
+
+        if posicion is None:
+            item = self.cola.data
+            if self.tamanio == 1:
+                self.cabeza = None
+                self.cola = None
+            else:
+                self.cola = self.cola.anterior
+                self.cola.siguiente = None
+        else:
+            if posicion < 0 or posicion >= self.tamanio:
+                raise IndexError("La posición dada no forma parte de la lista")
+            
+            actual = self.cabeza
+            for i in range(posicion):
+                actual = actual.siguiente
+
+            item = actual.data
+            if actual == self.cabeza:
+                self.cabeza = actual.siguiente
+            else:
+                actual.anterior.siguiente = actual.siguiente
+            if actual == self.cola:
+                self.cola = actual.anterior
+            else:
+                actual.siguiente.anterior = actual.anterior
+
+        self.tamanio -= 1
+        return item    
+    
+    def copiar(self):
+        nueva_lista = ListaDoblementeEnlazada()
+        actual = self.cabeza
+
+        while actual:
+            nuevo_nodo = Nodo(actual.data)
+            if nueva_lista.cabeza is None:
+                nueva_lista.cabeza = nuevo_nodo
+                nueva_lista.cola = nuevo_nodo
+            else:
+                nueva_lista.cola.siguiente = nuevo_nodo
+                nuevo_nodo.anterior = nueva_lista.cola
+                nueva_lista.cola = nuevo_nodo
+            actual = actual.siguiente
+
+        return nueva_lista
+
+    def invertir(self):
+        actual = self.cabeza
+        while actual:
+            siguiente = actual.siguiente
+            actual.siguiente = actual.anterior
+            actual.anterior = siguiente
+            actual = siguiente
+        self.cabeza, self.cola = self.cola, self.cabeza
+        return self
+
+    def concatenar(self, otra_lista):
+        if self.cola is None:
+            self.cabeza = otra_lista.cabeza
+            self.cola = otra_lista.cola
+        else:
+            self.cola.siguiente = otra_lista.cabeza
+            otra_lista.cabeza.anterior = self.cola
+            self.cola = otra_lista.cola
+
+        return self
+    def __add__(self, otra_lista):
+        nueva_lista = self.copiar()
+        nueva_lista.concatenar(otra_lista)
+        return nueva_lista
--- TrabajoPractico_1/Actividad_2/tests/test_LDE.py
+++ TrabajoPractico_1/Actividad_2/tests/test_LDE.py
@@ -4,7 +4,7 @@ Created on Thu Sep  1 14:00:21 2022
 @author: Catedra de Algoritmos y Estructura de Datos
 """
 
-from Modulos.ListasDoblementeEnlazadas import ListaDoblementeEnlazada
+from modules.ListasDoblementeEnlazadas import ListaDoblementeEnlazada
 import unittest
 import random
 
